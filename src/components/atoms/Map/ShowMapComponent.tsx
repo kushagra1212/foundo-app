fbef1a66e8158c6d8f63b196a321447756e0cc65
kushagra1212 <kushagrarathore002@gmail.com>
2023-01-26 02:47:18 +0530

    Revert "f"
    This reverts commit 93a93e5aaef9dd2af69d4111a9fd84dcada40aef.

--- src/components/atoms/Map/ShowMapComponent.tsx
+++ src/components/atoms/Map/ShowMapComponent.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { LeafletView } from 'react-native-leaflet-view';
 type props = {
   latitude: number;
   longitude: number;
@@ -7,7 +7,7 @@ type props = {
 const ShowMapComponent: React.FC<props> = ({ latitude, longitude }) => {
   return (
     <View style={{ flex: 1 }}>
-      <MapView
+      {/* <MapView
         region={{
           latitude: latitude,
           longitude: longitude,
@@ -22,7 +22,11 @@ const ShowMapComponent: React.FC<props> = ({ latitude, longitude }) => {
             longitude: longitude,
           }}
         />
-      </MapView>
+      </MapView> */}
+      <LeafletView
+        mapCenterPosition={[latitude, longitude]}
+        // The rest of your props, see the list below
+      />
     </View>
   );
 };
