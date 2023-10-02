e2330e1f916171fee9989dc5f60ff47fd779573a
kushagra <kushagrarathore002@gmail.com>
2023-10-03 03:21:28 +0530

    (fix): tests

--- run-tests.js
+++ run-tests.js
@@ -14,7 +14,7 @@ const testPatterns = [
 
 function runTests(pattern) {
   return new Promise((resolve, reject) => {
-    const command = `npm run jest -- --testPathPattern=${pattern} --forceExit`;
+    const command = `npm run jest -- --testPathPattern=${pattern} --forceExit --runInBand --verbose`;
     console.log(`Running tests for pattern: ${pattern}`);
     const childProcess = exec(command, (error, stdout, stderr) => {
       if (error) {
@@ -31,14 +31,29 @@ function runTests(pattern) {
 }
 
 (async () => {
+  let passedTests = 0;
   for (const pattern of testPatterns) {
     try {
       const stdout = await runTests(pattern);
       // show the colorized output of the tests in the terminal
       // console.log(chalk.green(stdout));
       console.log(chalk.green(`Tests for pattern ${pattern} passed.`));
+      passedTests++;
     } catch (error) {
       console.error(`Tests for pattern ${pattern} failed.`);
     }
+  }
+  if (passedTests === testPatterns.length) {
+    console.log(
+      chalk.green(
+        `All tests passed. ${passedTests} out of ${testPatterns.length} tests passed.`,
+      ),
+    );
+  } else {
+    console.log(
+      chalk.red(
+        `Tests failed. ${passedTests} out of ${testPatterns.length} tests passed.`,
+      ),
+    );
   }
 })();
